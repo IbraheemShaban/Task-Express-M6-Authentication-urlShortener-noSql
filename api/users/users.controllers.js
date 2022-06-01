dda5ee2e286c87e74e3ea011b4a27bdc4fae2d42
IbraheemShaban <ishaban097@gmail.com>
2022-06-01 20:31:02 +0300

    FINISHED the signup part

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,4 +1,5 @@
 const User = require('../../models/User');
+const bcrypt = require('bcrypt');
 
 exports.signin = async (req, res) => {
   try {
@@ -9,6 +10,8 @@ exports.signin = async (req, res) => {
 
 exports.signup = async (req, res) => {
   try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
     const newUser = await User.create(req.body);
     res.status(201).json(newUser);
   } catch (err) {
--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect('Your connection string', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+  const conn = await mongoose.connect(
+    'mongodb+srv://ishaban:Qwerty123@cluster0.ww6ulma.mongodb.net/test',
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  );
 
   console.log(`mongo connected: ${conn.connection.host}`);
 };
